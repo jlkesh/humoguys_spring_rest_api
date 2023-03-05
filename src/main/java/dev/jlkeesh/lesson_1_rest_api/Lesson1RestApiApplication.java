a7f52300f06533aae3d0f6d02c1d98bd88de68ea
pdp <john.lgd65@gmail.com>
2023-03-05 11:20:17 +0500

    initial

--- src/main/java/dev/jlkeesh/lesson_1_rest_api/Lesson1RestApiApplication.java
+++ src/main/java/dev/jlkeesh/lesson_1_rest_api/Lesson1RestApiApplication.java
@@ -1,9 +1,28 @@
 package dev.jlkeesh.lesson_1_rest_api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Study Project",
+                description = "This Project just for Learning Purpose",
+                termsOfService = "https://online.pdp.uz",
+                contact = @Contact(
+                        name = "Javohir Elmurodov",
+                        url = "https://github.com/jlkesh",
+                        email = "john@gmail.com"
+                )
+        ),
+        tags = {
+                @Tag(name = "Book-Controller-Tag", description = "You can do xzzzzzzzzzzzzzzzzzzzzzzzzzzzz")
+        }
+)
 public class Lesson1RestApiApplication {
 
     public static void main(String[] args) {
